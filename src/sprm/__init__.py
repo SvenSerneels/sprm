fe044cf7ba293d6dde5cc25479ce6d957f8d2471
Sven Serneels <svenserneels@gmail.com>
2019-01-26 20:31:31 -0500

    0.1.19 Preparing for version 0.2

--- src/sprm/__init__.py
+++ src/sprm/__init__.py
@@ -9,5 +9,13 @@ Created on Sun Jul 22 12:17:17 2018
 __name__ = "sprm"
 __author__ = "Sven Serneels"
 __license__ = "GNU"
-__version__ = "0.1.8"
-__date__ = "2019-01-19"
+__version__ = "0.1.19"
+__date__ = "2019-01-26"
+
+from .robcent import robcent
+from .sprm import snipls,sprm
+from .rm import rm
+
+
+
+
