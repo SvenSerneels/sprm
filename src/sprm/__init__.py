cd353ba8d9290043222c628c8df0bfd9b7127811
Sven Serneels <svenserneels@gmail.com>
2020-04-04 21:50:13 -0400

    Adieu sprm!

--- src/sprm/__init__.py
+++ src/sprm/__init__.py
@@ -9,8 +9,8 @@ Created on Sun Jul 22 12:17:17 2018
 __name__ = "sprm"
 __author__ = "Sven Serneels"
 __license__ = "MIT"
-__version__ = "0.7.0"
-__date__ = "2020-04-03"
+__version__ = "0.7.1"
+__date__ = "2020-04-04"
 
 from .preprocessing.robcent import VersatileScaler, versatile_scale
 from .preprocessing.gsspp import GenSpatialSignPrePprocessor, gen_ss_pp, gen_ss_covmat
